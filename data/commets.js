9323bd001401827c771be56839d9c3a6a15d0063
DeeonStjean <deeonstjean101@gmail.com>
2024-09-08 16:39:22 -0400

    adding data

---
+++ data/commets.js
@@ -0,0 +1,34 @@
+const comments =[
+    {
+        id: 1,
+        userId: 1,
+        comment: "est et quae odit qui non"
+    },
+    {
+        id: 2,
+        userId: 1,
+        comment: "quasi id et eos tenetur aut quo autem",
+    },
+    {
+        id: 3,
+        userId: 2,
+        comment: "Donec gravida id nisl eu egestas.",
+    },
+    {
+        id: 4,
+        userId: 2,
+        comment: "In fringilla elementum ipsum viverra bibendum. Donec ante velit",
+    },
+    {
+        id: 5,
+        userId: 3,
+        comment: "estibulum sem orci, interdum vel magna nec, mollis consectetur metus.",
+    },
+    {
+        id: 6,
+        userId: 3,
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+]
+
+module.exports = comments;
---
+++ data/users.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+      id: 1,
+      name: "Deeon",
+      username: "dmoney23",
+      email: "dmoney23@example.com",
+    },
+    {
+      id: 2,
+      name: "Peter",
+      username: "p_pokemonMaster",
+      email: "p_pm@example.com",
+    },
+    {
+      id: 3,
+      name: "Brian",
+      username: "Big_B",
+      email: "bmoney@example.com",
+    },
+];
+  
+module.exports = users;
